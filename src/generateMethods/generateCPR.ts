83abbc2866499790179fbcd1d2b3917d2e80a137
Alex <a.sandrovschii@gmail.com>
2022-03-29 00:11:00 +0200

    fixed date issue

--- src/generateMethods/generateCPR.ts
+++ src/generateMethods/generateCPR.ts
@@ -6,27 +6,31 @@ import {
 } from "../utils/generateRandomNumberBetweenRange";
 
 export const generateCPR = (person: Person | null, dateOfBirth: Date) => {
-    let personToGetCPR: Person;
-    let result: string;
-    // the following logic is stupid, 
-    // but I did it because it is a requirement to
-    // generate CPRs even without a person...
-    if (person === null) {
-        personToGetCPR = {
-            firstName: '',
-            lastName: '',
-            gender: (Date.now() % 2) == 1 ? 'male' : 'female',
-        }
-    } else {
-        personToGetCPR = person;
+  let personToGetCPR: Person;
+  let result: string;
+  // the following logic is stupid,
+  // but I did it because it is a requirement to
+  // generate CPRs even without a person...
+  if (person === null) {
+    personToGetCPR = {
+      firstName: "",
+      lastName: "",
+      gender: Date.now() % 2 == 1 ? "male" : "female"
     };
+  } else {
+    personToGetCPR = person;
+  }
 
-    const CPRDate = dateOfBirth.toLocaleDateString("en-GB", {
-        year: "2-digit",
-        month: "2-digit",
-        day: "2-digit",
-    }).replaceAll('/','');    
-    let randomNumber = personToGetCPR.gender === 'male' ? generateRandomOddNumber(0, 9999) : generateRandomEvenNumber(0, 9999);
-    result = `${CPRDate}-${padZeros(randomNumber, 4)}`;
-    return result;
+  const CPRDate = dateOfBirth
+    .toLocaleDateString("en-GB", {
+      year: "2-digit",
+      month: "2-digit",
+      day: "2-digit"
+    }).split("/").join("");
+  let randomNumber =
+    personToGetCPR.gender === "male"
+      ? generateRandomOddNumber(0, 9999)
+      : generateRandomEvenNumber(0, 9999);
+  result = `${CPRDate}-${padZeros(randomNumber, 4)}`;
+  return result;
 };
